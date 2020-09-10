cdb0be9443b2f1b86d51c2c0ba39d0ba9d2593e7
MyroslavM <merosalvchuk@gmail.com>
2020-09-10 21:30:10 +0300

    add routing to project

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -4,12 +4,14 @@ export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public id: number;
   public ingredients: Ingredient[ ];
 
-  constructor(name: string, desc: string, imagePath: string, ingredients : Ingredient[]) {
+  constructor(name: string, desc: string, imagePath: string, id: number, ingredients : Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
     this.ingredients = ingredients;
+    this.id = id;
   }
 }
